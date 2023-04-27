04318438b0ee40a84b6678454c889628bf49b7fb
Amir <amir.akbrzadh3@gmail.com>
2023-04-27 16:25:41 +0430

    change hero section arrow icon background color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,10 @@ let Hero = () => {
         <div className="container mx-auto mt-4 tablet:px-0 h-[10rem]" >
             <div className="relative overflow-hidden h-full bg-cover bg-[left_center]" style={{ backgroundImage: `url(${HeroImage})`,}}>
                 <div className="absolute w-full h-full top-0 left-0 text-2xl">
-                    <div className="absolute left-0 top-[50%] translate-y-[-50%] hover:pr-2 px-3 py-4 bg-[#252931] hover:bg-[#56595F] transation-all duration-[0.2s] cursor-pointer  rounded-r-full flex items-center justify-center">
+                    <div className="absolute left-0 top-[50%] translate-y-[-50%] px-3 py-4 bg-gray-800 opacity-[.6] hover:opacity-[1] transation-all duration-[0.2s] cursor-pointer  rounded-r-full flex items-center justify-center">
                         <LeftSideIcon color="white" />
                     </div>
-                    <div className="absolute right-0 top-[50%] translate-y-[-50%] hover:pr-2 px-3 py-4 bg-[#252931] hover:bg-[#56595F] transation-all duration-[0.2s] cursor-pointer  rounded-l-full flex items-center justify-center">
+                    <div className="absolute right-0 top-[50%] translate-y-[-50%] px-3 py-4 bg-gray-800 opacity-[.6] hover:opacity-[1] transation-all duration-[0.2s] cursor-pointer  rounded-l-full flex items-center justify-center">
                         <RightSideIcon color="white" />
                     </div>
                 </div>
