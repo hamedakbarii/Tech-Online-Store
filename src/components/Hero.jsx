42dadfb7e41b59ded59e3ceb0d153950ae722209
hamedakbari <hamedakbariwork@gmail.com>
2023-12-25 00:03:41 +0330

    mobile design updated and checkout router component updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,23 @@
-import { LeftSideIcon , RightSideIcon } from "./Icon";
-import HeroImage from '../hero-banner.png' ; 
+import { LeftSideIcon, RightSideIcon } from "./Icon";
+import HeroImage from "../hero-banner.png";
+
 let Hero = () => {
-    return (
-        <div className="container mx-auto mt-4 tablet:px-0 h-[10rem]" >
-            <div className="relative overflow-hidden h-full bg-cover bg-[left_center]" style={{ backgroundImage: `url(${HeroImage})`,}}>
-                <div className="absolute w-full h-full top-0 left-0 text-2xl">
-                    <div className="absolute left-0 top-[50%] translate-y-[-50%] px-3 py-4 bg-gray-800 opacity-[.6] hover:opacity-[1] transation-all duration-[0.2s] cursor-pointer  rounded-r-full flex items-center justify-center">
-                        <LeftSideIcon color="white" />
-                    </div>
-                    <div className="absolute right-0 top-[50%] translate-y-[-50%] px-3 py-4 bg-gray-800 opacity-[.6] hover:opacity-[1] transation-all duration-[0.2s] cursor-pointer  rounded-l-full flex items-center justify-center">
-                        <RightSideIcon color="white" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container mx-auto mt-4 tablet:px-0 h-[10rem]">
+      <div
+        className="relative overflow-hidden h-full bg-cover bg-[left_center]"
+        style={{ backgroundImage: `url(${HeroImage})` }}
+      >
+        <div className="absolute w-full h-full top-0 left-0 text-2xl">
+          <div className="absolute left-0 top-[50%] translate-y-[-50%] px-3 py-4 bg-gray-800 opacity-[.6] hover:opacity-[1] transation-all duration-[0.2s] cursor-pointer  rounded-r-full flex items-center justify-center">
+            <LeftSideIcon color="white" />
+          </div>
+          <div className="absolute right-0 top-[50%] translate-y-[-50%] px-3 py-4 bg-gray-800 opacity-[.6] hover:opacity-[1] transation-all duration-[0.2s] cursor-pointer  rounded-l-full flex items-center justify-center">
+            <RightSideIcon color="white" />
+          </div>
         </div>
-    )
-}
-export default Hero ; 
+      </div>
+    </div>
+  );
+};
+export default Hero;
--- src/components/UserFeedBack.jsx
+++ src/components/UserFeedBack.jsx
@@ -17,7 +17,7 @@ let UserFeedBackData = [
     message: `My first order arrived today in perfect condition.  
         From the time I sent a question about the item to making the purchase, to the shipping and now the delivery, your company, Tecs, has stayed in touch.  Such great service.
         I look forward to shopping on your site in the future and would highly recommend it.`,
-    user: "maji amini",
+    user: "majid amini",
   },
   {
     message: `My first order arrived today in perfect condition.  
@@ -46,6 +46,7 @@ let UserFeedBack = () => {
         className="relative"
       >
         <span className="w-full h-full top-0 left-0 absolute bg-[#eff5ff] rounded-lg"></span>
+
         {UserFeedBackData &&
           UserFeedBackData.map((item) => (
             <SwiperSlide className="relative p-4 px-6 rounded-lg">
@@ -53,10 +54,12 @@ let UserFeedBack = () => {
                 <p className='flex items-start text-md text-justify before:block before:content-["“"] before:leading-[.7] before:mr-4 before:text-6xl'>
                   {item.message}
                 </p>
+
                 <span className='flex items-center justify-end mt-4 before:block before:content-["-"] before:mr-2 text-lg font-semibold'>
                   {item.user}
                 </span>
-                <button className="z-20 text-secondaryBlue border-2 border-secondaryBlue w-max py-1 px-4 mt-6 text-lg font-semibold rounded-full transation-all duration-[.2s] hover:bg-secondaryBlue hover:text-white">
+
+                <button className="text-secondaryBlue border-2 border-secondaryBlue w-max p-1 mt-6 text-lg font-semibold rounded-full transation-all duration-[.2s] hover:bg-secondaryBlue hover:text-white">
                   Leave Us A Review
                 </button>
               </div>
@@ -67,37 +70,3 @@ let UserFeedBack = () => {
   );
 };
 export default UserFeedBack;
-/*
-// import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
-export default () => {
-  return (
-    <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
-  );
-};
-*/
